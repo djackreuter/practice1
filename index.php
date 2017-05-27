dac5fb0a19d0f3feac716a6a6e074362823ff3bf
Dylan Reuter <djreuter45@gmail.com>
2017-05-26 20:11:26 -0600

    created practice page one index

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Practice Number One</title>
+	</head>
+	<body>
+		<div class="header">
+			<div id="branding">
+		<h1>Practice Page</h1>
+			</div>
+			<nav>
+				<ul>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">About</a></li>
+					<li><a href ="#">Services</a></li>
+					<li><a href ="#">Contact</a></li>
+				</ul>
+			</nav>
+		</div>
+		<main>
+			<p>Suspendisse nec nisi ac felis egestas rhoncus. Morbi turpis est, sagittis a viverra vitae, tempus id augue.
+				Aenean vitae sagittis orci, a suscipit nulla. Donec vulputate, purus sit amet venenatis pharetra, mauris nibh
+				rhoncus arcu, ac aliquam felis lectus sit amet risus. Nullam suscipit non sem nec luctus. Nulla in sem id augue
+				tincidunt aliquam. Curabitur metus odio, venenatis sed lacus ut, venenatis faucibus libero. Nullam nec eros
+				finibus, aliquet orci vel, egestas nunc.Mauris laoreet dictum mollis. Curabitur nec convallis mi, vel blandit
+				nibh. Morbi sodales porttitor nisi, sit amet dapibus lectus hendrerit sit amet. Nullam molestie ullamcorper
+				ornare.
+			</p>
+			<img src="http://wallpaper-gallery.net/images/picture/picture-14.jpg" alt="A monkey smiling">
+			<p>Click <a href="#" target="_blank">HERE</a> to see more pictures of smiling monkeys.</p>
+		</main>
+
+
+	</body>
+</html>
